c18781dd26dc9f1e2844469734c0ec1c73d0bcf5
reza2boyce <reza2boyce@gmail.com>
2022-05-07 15:31:44 +0430

    add audio effects

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const cpuHandFrame = document.getElementById("cpuHandFrame");
-const playerHandFrame = document.getElementById("playerHandFrame");
+const cpuHandFrame = document.getElementById("cpuHandFrame"); //get cpu hand element dom
+const playerHandFrame = document.getElementById("playerHandFrame"); //get player hand element dom
+let score = 0; //the score vlaue
+let hightScore = 0; //the higthScore value
 
 //create hand image
 function createHand(str, element) {
-  const buildElement = document.createElement("img");
-  let imageAddress = "";
+  const buildElement = document.createElement("img"); //create element img
+  let imageAddress = "";  //the file address
 
   if (str == "stone") {
     imageAddress = "library/stone.png";
@@ -14,9 +16,9 @@ function createHand(str, element) {
     imageAddress = "library/scissors.png";
   }
 
-  buildElement.src = imageAddress;
-  buildElement.classList.add("hand-pic");
-  element.appendChild(buildElement);
+  buildElement.src = imageAddress;  //set srt image to created img element
+  buildElement.classList.add("hand-pic"); //add class to element
+  element.appendChild(buildElement); //append finally element to own div
 }
 
 //cpu get hand
@@ -75,24 +77,47 @@ function checkHand(srcValue) {
         isWin = false;
     }
 
-
-    //finaly 
-    if(isDraw == true) {
-        console.log('You Draw !');
+    //final resault
+    if(isDraw == true) {  //if Draw
+        document.getElementById('status').innerHTML = 'You Draw !';
+        playAudioEffect('library/audio/draw.mp3');
     }else if(isDraw == false && isTrue == true){
-        console.log('You Win !');
-    }else if(isDraw == false && isTrue == false){
-        console.log('You lose !');
+        document.getElementById('status').innerHTML = 'You Win !'; //if Win
+        playAudioEffect('library/audio/winAudio.wav'); //play audio effect for win
+        score++;
+        if(score > hightScore) {
+          hightScore = score;
+          document.getElementById('higthScore').innerHTML = hightScore;
+        }
+        document.getElementById('score').innerHTML = score;
+    }else if(isDraw == false && isTrue == false){  //if lose
+        document.getElementById('status').innerHTML = 'You Lose !';
+        playAudioEffect('library/audio/loseAudio.wav'); //play audio effect for lose
+        resetScore();
+        document.getElementById('score').innerHTML = score;
     }
 
-    cpuHandFrame.innerHTML = '';
-    cpuGetHand();
+    cpuHandFrame.innerHTML = ''; //reset the content of div
+    cpuGetHand(); //random again
+}
+
+//play audio
+function playAudioEffect(src){
+  let audio = new Audio('');
+  audio = new Audio(src);
+  audio.play();
+}
+
+//resetSore
+function resetScore(){
+  score = 0;
 }
 
 //when page loade completed
 window.onload = () => {
   //cpu get hand
   cpuGetHand();
+  document.getElementById('score').innerHTML = 0;
 };
 
 //create user hand
@@ -100,7 +125,7 @@ createHand("stone", playerHandFrame);
 createHand("paper", playerHandFrame);
 createHand("scissors", playerHandFrame);
 
-//set event to hands
+//set event to hands, get url src from img elements
 const handPlayerElement = document.getElementById('playerHandFrame').children;
 handPlayerElement[0].addEventListener('click', (e) => checkHand(e.target.src));
 handPlayerElement[1].addEventListener('click', (e) => checkHand(e.target.src));
